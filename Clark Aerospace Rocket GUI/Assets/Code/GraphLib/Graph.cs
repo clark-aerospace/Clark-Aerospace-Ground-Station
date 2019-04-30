46fd582f7dfe111c6113ddd90871cec11849eafe
malcolminyo <malcolminyo@gmail.com>
2019-04-30 14:08:54 -0700

    graph system working better

--- Clark Aerospace Rocket GUI/Assets/Code/GraphLib/Graph.cs
+++ Clark Aerospace Rocket GUI/Assets/Code/GraphLib/Graph.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
 using TMPro;
 
 public class Graph : MonoBehaviour
@@ -12,9 +13,9 @@ public class Graph : MonoBehaviour
         Y = 1
     }
     private List<Vector2> points = new List<Vector2>();
-    
-    public LineRenderer lineRenderer;
-    public LineRenderer gridLineRenderer;
+
+    public UILineRenderer uiLineRenderer;
+    public UIGridRenderer gridRenderer;
 
     [SerializeField]
     private RectTransform rectTransform;
@@ -41,29 +42,28 @@ public class Graph : MonoBehaviour
 
     int thing = 0;
 
+    public float xSpacePerLine = 1;
+    public float ySpacePerLine = 1;
+
+    [Header("Labels")]
+    public TextMeshProUGUI xAxisMaxLabel;
+    public TextMeshProUGUI xAxisMinLabel;
+    public TMP_FontAsset defaultTextFont;
+
+    public RectTransform scrolleyThing;
+    public TextMeshProUGUI scrolleyThing_Label;
+
+
 
     public void Start() {
-        lineRenderer = new GameObject().AddComponent<LineRenderer>();
-        lineRenderer.transform.SetParent(transform, false);
 
-        lineRenderer.numCornerVertices = 8;
-        lineRenderer.numCapVertices = 8;
-        lineRenderer.SetWidth(3f, 3f);
-        lineRenderer.alignment = LineAlignment.TransformZ;
+        uiLineRenderer = new GameObject().AddComponent<UILineRenderer>();
+        uiLineRenderer.transform.SetParent(transform, false);
+        uiLineRenderer.LineThickness = 3;
 
-        lineRenderer.useWorldSpace = false;
 
-        gridLineRenderer = new GameObject().AddComponent<LineRenderer>();
-        gridLineRenderer.transform.SetParent(transform, false);
-        gridLineRenderer.material = new Material(Shader.Find("Unlit/Color"));
-        gridLineRenderer.material.color = new Color(0.7f, 0.7f, 0.7f, 0.5f);
-        gridLineRenderer.transform.localPosition = new Vector3(0f, 0f, 1f);
-        //vertLinesRenderer.numCornerVertices = 8;
-        //vertLinesRenderer.numCapVertices = 8;
-        //vertLinesRenderer.SetWidth(3f, 3f);
-        gridLineRenderer.alignment = LineAlignment.TransformZ;
-
-        gridLineRenderer.useWorldSpace = false;
+        gridRenderer = gameObject.AddComponent<UIGridRenderer>();
+        gridRenderer.color = new Color(0.5f, 0.5f, 0.5f);
 
         rectTransform = GetComponent<RectTransform>();
 
@@ -81,6 +81,33 @@ public class Graph : MonoBehaviour
 
         mesh = new Mesh();
         mFilter.mesh = mesh;
+
+        // Create the X axis Max label
+        xAxisMaxLabel = new GameObject().AddComponent<TextMeshProUGUI>();
+        xAxisMaxLabel.rectTransform.SetParent(transform, false);
+        xAxisMaxLabel.rectTransform.pivot = new Vector2(1f,1f);
+        xAxisMaxLabel.rectTransform.anchorMax = new Vector2(1f,0f);
+        xAxisMaxLabel.rectTransform.anchorMin = new Vector2(1f,0f);
+        xAxisMaxLabel.rectTransform.anchoredPosition = new Vector2(0f, 0f);
+        xAxisMaxLabel.rectTransform.sizeDelta = new Vector2(50,25);
+        xAxisMaxLabel.font = defaultTextFont;
+        xAxisMaxLabel.fontSize = 15;
+        xAxisMaxLabel.text = "Hello!";
+        xAxisMaxLabel.alignment = TextAlignmentOptions.MidlineRight;
+
+
+        // Create the X axis Min label
+        xAxisMinLabel = new GameObject().AddComponent<TextMeshProUGUI>();
+        xAxisMinLabel.rectTransform.SetParent(transform, false);
+        xAxisMinLabel.rectTransform.pivot = new Vector2(0f,1f);
+        xAxisMinLabel.rectTransform.anchorMax = new Vector2(0f,0f);
+        xAxisMinLabel.rectTransform.anchorMin = new Vector2(0f,0f);
+        xAxisMinLabel.rectTransform.anchoredPosition = new Vector2(0f, 0f);
+        xAxisMinLabel.rectTransform.sizeDelta = new Vector2(50,25);
+        xAxisMinLabel.font = defaultTextFont;
+        xAxisMinLabel.fontSize = 15;
+        xAxisMinLabel.text = "Hello!";
+        xAxisMinLabel.alignment = TextAlignmentOptions.MidlineLeft;
 
 
         AddPoint(0, 0);
@@ -121,7 +148,10 @@ public class Graph : MonoBehaviour
         SetLineColor();
         OnMouseOver();
         GenerateMesh();
-        DrawGridLines();
+        GetTickInterval();
+
+        xAxisMinLabel.text = "0";
+        xAxisMaxLabel.text = (gridRenderer.GridColumns * xSpacePerLine).ToString();
     }
 
     public void AddTestPoint() {
@@ -135,8 +165,7 @@ public class Graph : MonoBehaviour
 
     public void GenerateMesh() {
 
-        Vector3[] _pts = new Vector3[lineRenderer.positionCount];
-        lineRenderer.GetPositions(_pts);
+        Vector3[] _pts = Vec2ArrayToVec3Array(uiLineRenderer.Points);
 
         Vector2[] pts = ScaledPoints(Vec3ArrayToVec2Array(_pts));
         pts = points.ToArray();
@@ -194,8 +223,7 @@ public class Graph : MonoBehaviour
     }
 
     public void SetLineColor() {
-        lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
-        lineRenderer.material.color = color;
+        uiLineRenderer.color = color;
         dotImage.color = color;
 
     }
@@ -219,8 +247,16 @@ public class Graph : MonoBehaviour
             float xPercent = (mousePos.x + (rectTransform.pivot.x * rectTransform.rect.width)) / rectTransform.rect.width;
 
             float xHover = (xPercent * GetMaxValue(Axis.X).x);
+            dotImage.rectTransform.localScale = Vector2.one;
+            scrolleyThing.localScale = Vector3.one;
             dotImage.rectTransform.anchoredPosition = ScalePoint(GetInterpolatedValue(xHover)); //ScalePoint(ValueAtX(xHover));
             Debug.Log(xHover.ToString());
+            scrolleyThing.anchoredPosition = ScalePoint(GetInterpolatedValue(xHover)) + new Vector2(0f, 3f);
+        }
+
+        else {
+            dotImage.rectTransform.localScale = Vector2.zero;
+            scrolleyThing.localScale = Vector3.zero;
         }
     }
 
@@ -302,129 +338,170 @@ public class Graph : MonoBehaviour
     }
 
     private void ResetLineRenderer() {
-
-        for (int i = 0; i < lineRenderer.positionCount; i++) {
-            lineRenderer.SetPosition(i, Vector3.zero);
-        }
-        lineRenderer.positionCount = points.Count;
-        
-        Vector3[] array = Vec2ArrayToVec3Array(ScaledPoints(points.ToArray()));
-
-        lineRenderer.SetPositions(array);
-
-        lineRenderer.Simplify((GetMaxValue(Axis.Y).y - GetMinValue(Axis.Y).y) * 0.01f);
-        
-
+        uiLineRenderer.Points = ScaledPoints(points.ToArray());
     }
 
-    public void DrawGridLines() {
-        // The line needs to start at the TOP LEFT corner (min x, max y) and go to
-        // the TOP RIGHT corner (max x, max y). THEN, move one unit down (max x, max y - 1)
-        // and then back to (min x, max y - 1)
+    public void GetTickInterval() {
+        float xRange = GetMaxValue(Axis.X, false).x - GetMinValue(Axis.X, false).x;
 
-        Vector3 startPos = new Vector3(GetMinValue(Axis.X, true).x, GetMaxValue(Axis.Y, true).y, 0f);
+        // We now have the range of X values. Let's find the CLOSEST number
+        // to it that fits a good number (power of 1, 5, or 10).
 
+        xSpacePerLine = CalcStepSize(xRange, 8f);
+        Debug.Log("Step size is " + xSpacePerLine.ToString());
 
+        // the number of steps would be 
+    
+        float xTicks = xRange / xSpacePerLine;
 
-        Vector3 endPos = new Vector3(GetMaxValue(Axis.X, true).x, GetMaxValue(Axis.Y, true).y, 0f);
-
-
-
-        int numberOfVertsY = ((int)(GetMaxValue(Axis.Y).y) * 2) + 2;
-
-        int numberOfVertsX = ((int)(GetMaxValue(Axis.X).x) * 2) + 1;
-
-        int numberOfVerts = numberOfVertsY + numberOfVertsX;
+        gridRenderer.GridColumns = (int)xTicks;
 
 
-        gridLineRenderer.positionCount = numberOfVerts;
-        //print(numberOfVerts);
-        Vector3[] pos = new Vector3[numberOfVerts];
-        pos[0] = startPos;
-
-        Debug.Log(ScalePoint(Vector2.one));
-
-        float yUnitAmt = GetYStep();
-        float xUnitAmt = GetXStep();
-        Debug.Log("Y unit amt: " + yUnitAmt.ToString());
-
-        int status = 0;
-        // 0 = moving right
-        // 1 = moving down after having moved right
-        // 2 = moving left
-        // 3 = moving down after having moved left
-
-        Vector3 currentPos = new Vector3(GetMinValue(Axis.X, true).x, GetMaxValue(Axis.Y, true).y, 0f);
-
-        for (int i = 1; i < numberOfVertsY; i++) {
-            //Debug.Log("loop iteration " + i.ToString());
-            if (status == 0) {
-                //Debug.Log("moving right");
-                currentPos.x = GetMaxValue(Axis.X, true).x;
-                status = 1;
-            }
-
-            else if (status == 1) {
-                //Debug.Log("moving down");
-                currentPos.y -= yUnitAmt;
-                status = 2;
-            }
-
-            else if (status == 2) {
-                //Debug.Log("moving left");
-                currentPos.x = GetMinValue(Axis.X, true).x;
-                status = 3;
-            } else if (status == 3) {
-                //Debug.Log("moving down");
-                currentPos.y -= yUnitAmt;
-                status = 0;
-            }
-
-            pos[i] = currentPos;
-        }
-
-        // the Ys are taken care of, now for the Xs
-
-        status = 0;
-        // 0 = moving up
-        // 1 = moving left after having moved up
-        // 2 = moving down
-        // 3 = moving left after having moved down
-
-        for (int i = numberOfVertsY; i < numberOfVerts; i++) {
-            if (status == 0) {
-                currentPos.y = GetMaxValue(Axis.Y, true).y;
-                status = 1;
-            }
-
-            else if (status == 1) {
-                currentPos.x -= xUnitAmt;
-                status = 2;
-            }
-
-            else if (status == 2) {
-                currentPos.y = GetMinValue(Axis.Y, true).y;
-                status = 3;
-            }
-
-            else if (status == 3) {
-                currentPos.x -= xUnitAmt;
-                status = 0;
-            }
-
-            pos[i] = currentPos;
-        }
-
-        gridLineRenderer.SetPositions(pos);
-        gridLineRenderer.startWidth = 1f;
-        gridLineRenderer.endWidth = 1f;
-
-
-
-        // gridLineRenderer.startColor = new Color(0.3f, 0.3f, 0.3f, 0.7f);
-        // gridLineRenderer.endColor = new Color(0.3f, 0.3f, 0.3f, 0.7f);
-
+        float yRange = GetMaxValue(Axis.Y, false).y - GetMinValue(Axis.Y, false).y;
+        ySpacePerLine = CalcStepSize(yRange, 5f);
+        float yTicks = yRange / ySpacePerLine;
+        gridRenderer.GridRows = (int)yTicks;
     }
+
+    public static float CalcStepSize(float range, float targetSteps)
+    {
+        // calculate an initial guess at step size
+        var tempStep = range/targetSteps;
+
+        // get the magnitude of the step size
+        var mag = (float)Mathf.Floor(Mathf.Log10(tempStep));
+        var magPow = (float)Mathf.Pow(10, mag);
+
+        // calculate most significant digit of the new step size
+        var magMsd = (int)(tempStep/magPow + 0.5);
+
+        // promote the MSD to either 1, 2, or 5
+        if (magMsd > 5)
+            magMsd = 10;
+        else if (magMsd > 2)
+            magMsd = 5;
+        else if (magMsd > 1)
+            magMsd = 2;
+
+        return magMsd*magPow;
+    }
+
+    // public void DrawGridLines() {
+    //     // The line needs to start at the TOP LEFT corner (min x, max y) and go to
+    //     // the TOP RIGHT corner (max x, max y). THEN, move one unit down (max x, max y - 1)
+    //     // and then back to (min x, max y - 1)
+
+    //     Vector3 startPos = new Vector3(GetMinValue(Axis.X, true).x, GetMaxValue(Axis.Y, true).y, 0f);
+
+
+
+    //     Vector3 endPos = new Vector3(GetMaxValue(Axis.X, true).x, GetMaxValue(Axis.Y, true).y, 0f);
+
+
+
+    //     int numberOfVertsY = ((int)(GetMaxValue(Axis.Y).y) * 2) + 2;
+
+    //     int numberOfVertsX = ((int)(GetMaxValue(Axis.X).x) * 2) + 1;
+
+    //     int numberOfVerts = numberOfVertsY + numberOfVertsX;
+
+
+    //     gridLineRenderer.positionCount = numberOfVerts;
+    //     //print(numberOfVerts);
+    //     Vector3[] pos = new Vector3[numberOfVerts];
+    //     pos[0] = startPos;
+
+    //     Debug.Log(ScalePoint(Vector2.one));
+
+    //     float yUnitAmt = GetYStep();
+    //     float xUnitAmt = GetXStep();
+    //     Debug.Log("Y unit amt: " + yUnitAmt.ToString());
+
+    //     int status = 0;
+    //     // 0 = moving right
+    //     // 1 = moving down after having moved right
+    //     // 2 = moving left
+    //     // 3 = moving down after having moved left
+
+    //     Vector3 currentPos = new Vector3(GetMinValue(Axis.X, true).x, GetMaxValue(Axis.Y, true).y, 0f);
+
+    //     for (int i = 1; i < numberOfVertsY; i++) {
+    //         //Debug.Log("loop iteration " + i.ToString());
+    //         if (status == 0) {
+    //             //Debug.Log("moving right");
+    //             currentPos.x = GetMaxValue(Axis.X, true).x;
+    //             status = 1;
+    //         }
+
+    //         else if (status == 1) {
+    //             //Debug.Log("moving down");
+    //             currentPos.y -= yUnitAmt;
+    //             status = 2;
+    //         }
+
+    //         else if (status == 2) {
+    //             //Debug.Log("moving left");
+    //             currentPos.x = GetMinValue(Axis.X, true).x;
+    //             status = 3;
+    //         } else if (status == 3) {
+    //             //Debug.Log("moving down");
+    //             currentPos.y -= yUnitAmt;
+    //             status = 0;
+    //         }
+
+    //         pos[i] = currentPos;
+    //     }
+
+    //     bool endedOnRight;
+
+    //     if (status == 0) {
+    //         endedOnRight = true;
+    //     } else {
+    //         endedOnRight = false;
+    //     }
+
+    //     // the Ys are taken care of, now for the Xs
+
+    //     status = 0;
+    //     // 0 = moving up
+    //     // 1 = moving left after having moved up
+    //     // 2 = moving down
+    //     // 3 = moving left after having moved down
+
+    //     for (int i = numberOfVertsY; i < numberOfVerts; i++) {
+    //         if (status == 0) {
+    //             currentPos.y = GetMaxValue(Axis.Y, true).y;
+    //             status = 1;
+    //         }
+
+    //         else if (status == 1) {
+    //             currentPos.x += endedOnRight ? xUnitAmt : -(xUnitAmt);
+    //             status = 2;
+    //         }
+
+    //         else if (status == 2) {
+    //             currentPos.y = GetMinValue(Axis.Y, true).y;
+    //             status = 3;
+    //         }
+
+    //         else if (status == 3) {
+    //             currentPos.x += endedOnRight ? -xUnitAmt : (xUnitAmt);
+    //             status = 0;
+    //         }
+
+    //         pos[i] = currentPos;
+    //     }
+
+    //     gridLineRenderer.SetPositions(pos);
+    //     gridLineRenderer.startWidth = 1f;
+    //     gridLineRenderer.endWidth = 1f;
+
+
+
+    //     // gridLineRenderer.startColor = new Color(0.3f, 0.3f, 0.3f, 0.7f);
+    //     // gridLineRenderer.endColor = new Color(0.3f, 0.3f, 0.3f, 0.7f);
+
+    // }
     
     public float GetYStep() {
         // Return the world-space units that correspond to one graph-unit on the Y axis
